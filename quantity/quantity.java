4d99aa75102ebf6f83b862a48b9f46087f3454ea
James Ferguson <james@convexfunction.com>
2016-10-11 14:38:54 -0400

    etc

--- quantity/quantity.java
+++ quantity/quantity.java
@@ -1,12 +1,12 @@
 import java.lang.System;
 
-
 public class Quantity {
 
     public static void main(String[] args) {
-        Quantity q = new Quantity(3.0, "meters");
+        Quantity q1 = new Quantity(3.0, "meters");
         Quantity q2 = new Quantity(4.0, "meters");
-        System.out.println(q.toString() + " * " + q2.toString() + " = " + q.multiply(q2).toString());
+        Quantity answer = q1.multiply(q2)
+        System.out.println(q1.toString() + " * " + q2.toString() + " = " + answer.toString());
     }
-    // division, addition, exponentation, ...
+    // division, addition/subtraction, exponentiation, ...
 }
