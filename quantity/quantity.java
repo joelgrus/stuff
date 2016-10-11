0a4dd5c901481ce0cd017b6ca83f3079e87d8f46
James Ferguson <james@convexfunction.com>
2016-10-11 14:41:03 -0400

    semicolon

--- quantity/quantity.java
+++ quantity/quantity.java
@@ -5,7 +5,7 @@ public class Quantity {
     public static void main(String[] args) {
         Quantity q1 = new Quantity(3.0, "meters");
         Quantity q2 = new Quantity(4.0, "meters");
-        Quantity answer = q1.multiply(q2)
+        Quantity answer = q1.multiply(q2);
         System.out.println(q1.toString() + " * " + q2.toString() + " = " + answer.toString());
     }
     // division, addition/subtraction, exponentiation, ...
