98e837fd49f578cdc97dd44161d7e5b3d05326bb
James Ferguson <james@convexfunction.com>
2016-10-28 11:06:02 -0400

    clarify expected output

--- quantity/quantity.py
+++ quantity/quantity.py
@@ -4,24 +4,26 @@ class Quantity(object):
     pass
 
 
-
 q = Quantity
 
 def test_multiply():
     length = q('meters', 3)
     width = q('meters', 4)
     print '{} * {} = {}'.format(length, width, length * width)
+    # 12 * meters^2
 
 
 def test_divide():
     users = q('users', 10**4)
     req_per_user_day = q('requests', 5) / (q('users', 1) * q('day', 1))
     print '{} * {} = {}'.format(users, req_per_user_day, users * req_per_user_day)
+    # 50000 * requests * day^-1
 
 
 def test_power():
     length = q('millimeters', 5)
     print '{}^3 = {}'.format(length, length**3)
+    # 125 * millimeters^3
 
 
 def test_addition():
