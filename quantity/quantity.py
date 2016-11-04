8c737233919f4b5613dc35f1e5b1db2fefd41a3b
James Ferguson <james@convexfunction.com>
2016-11-04 11:52:17 -0400

    better tests, better implementation

--- quantity/quantity.py
+++ quantity/quantity.py
@@ -10,7 +10,13 @@ def test_multiply():
     length = q('meters', 3)
     width = q('meters', 4)
     print '{} * {} = {}'.format(length, width, length * width)
-    # 12 * meters^2
+    # (something similar to) 12 * meters^2
+
+
+def test_power():
+    length = q('millimeters', 5)
+    print '{}^3 = {}'.format(length, length**3)
+    # 125 * millimeters^3
 
 
 def test_divide():
@@ -20,21 +26,31 @@ def test_divide():
     # 50000 * requests * day^-1
 
 
-def test_power():
-    length = q('millimeters', 5)
-    print '{}^3 = {}'.format(length, length**3)
-    # 125 * millimeters^3
+def test_usability():
+    scrapes_per_site = 10 * q('scrapes') / q('site')
+    sites_per_day = 5000 * q('sites') / q('day')
+    # assume we're CPU bound
+    core_seconds_per_scrape = 15 * q('core') * q('seconds') / q('scrape')
+    core_time_per_day = scrapes_per_site * sites_per_day * core_seconds_per_scrape
+    seconds_per_day = 86000 * q('seconds') / q('day')
+    cores_needed = core_time_per_day / seconds_per_day
+
+    cores_per_instance = 4 * q('cores') / q('c3.xlarge')
+    instances_needed = cores_needed / cores_per_instance
+    import math
+    instances_deployed = math.ceil(instances_needed.magnitude) * q(instances_needed.units)
+    utilization = instances_needed / instances_deployed
+    print 'need {} ({}) for {} (utilization: {})'.format(
+                cores_needed,
+                instances_deployed,
+                scrapes_per_site * sites_per_day,
+                utilization)
 
 
-def test_addition():
-    print q('pencils', 2) + q('pencils', 3)
-    try:
-        print q('pencils', 2) + q('pens', 3)
-    except Exception as e:
-        print e
+# TODO what else could we define?
 
 
 test_multiply()
-test_divide()
-test_power()
-test_addition()
+# test_power()
+# test_divide()
+# test_usability()
--- quantity/solution/quantity.py
+++ quantity/solution/quantity.py
@@ -1,44 +1,40 @@
 # cheating isn't cool
 
-
 from __future__ import division
 
 
 class Quantity(object):
 
-    # tempting to set self.dimension to a string at first
-    def __init__(self, dimension, magnitude=None):
-        self.magnitude = magnitude
-        if isinstance(dimension, basestring):
-            self.dimension = {dimension: 1}
+    # tempting to set self.units to a string at first
+    def __init__(self, units, magnitude=1):
+        self.magnitude = float(magnitude)
+        if isinstance(units, basestring):
+            self.units = {units.rstrip('s'): 1}
         else:
-            self.dimension = {u: v for u, v in dimension.items() if v != 0}
-
+            self.units = {u.rstrip('s'): v for u, v in units.items() if v != 0}
 
     # generally have to tell people about __mul__ and its friends
     # though they could also look at set(dir(float)) - set(dir(object))
     def __mul__(a, b):
         if isinstance(b, Quantity):
-            magnitude = (a.magnitude or 1) * (b.magnitude or 1)
-            dimension = {u: a.dimension.get(u, 0) + b.dimension.get(u, 0)
-                         for u in a.dimension.keys() + b.dimension.keys()}
-            return Quantity(dimension, magnitude)
+            magnitude = a.magnitude * b.magnitude
+            units = {u: a.units.get(u, 0) + b.units.get(u, 0)
+                         for u in a.units.keys() + b.units.keys()}
+            return Quantity(units, magnitude)
         else:
-            return Quantity(a.dimension, (a.magnitude or 1) * b)
+            return Quantity(a.units, a.magnitude * b)
 
     # commutative
     __rmul__ = __mul__
-
 
     # easier to define division as ^-1
     def __pow__(a, b):
         if isinstance(b, int):
             magnitude = a.magnitude ** b
-            dimension = {d: a.dimension[d] * b for d in a.dimension}
-            return Quantity(dimension, magnitude)
+            units = {d: a.units[d] * b for d in a.units}
+            return Quantity(units, magnitude)
         else:
             raise TypeError("no")
-
 
     def __div__(a, b):
         return a * b**-1
@@ -47,14 +43,12 @@ class Quantity(object):
     def __rdiv__(b, a):
         return a * b**-1
 
-
     def __add__(a, b):
-        if a.dimension != b.dimension:
-            raise ValueError("only add things with the same dimensions :(")
-        return Quantity(a.dimension, a.magnitude + b.magnitude)
+        if a.units != b.units:
+            raise ValueError("only add things with the same units :(")
+        return Quantity(a.units, a.magnitude + b.magnitude)
 
     __radd__ = __add__
-
 
     def __sub__(a, b):
         return a + -1*b
@@ -62,14 +56,13 @@ class Quantity(object):
     def __rsub__(b, a):
         return a + -1*b
 
+    def __eq__(a, b):
+        return (a.magnitude == b.magnitude) and (a.units == b.units)
 
-    def __repr__(self):
-        numerator_dimension = ' * '.join(u + ('^{}'.format(p) if p > 1 else '')
-                                     for u, p in self.dimension.items() if p > 0)
-        denominator_dimension = ' * '.join(u + ('^{}'.format(p) if p < -1 else '')
-                                     for u, p in self.dimension.items() if p < 0)
-        r = '{:.2e} {}'.format(self.magnitude, numerator_dimension)
-        if denominator_dimension:
-            length = max(map(len, (r, denominator_dimension)))
-            r += '\n{}\n{}'.format('-'*length, denominator_dimension)
-        return r
+    def __str__(self):
+        units = [dim + ('s' if power > 0 else '') + (('^' + str(power)) if power != 1 else '')
+                 for dim, power in sorted(self.units.items(), key=lambda x:x[1], reverse=True)]
+        magnitude_format = ('{:.0f}' if self.magnitude.is_integer()
+                            else '{:.2f}' if (.01 < self.magnitude < 10000)
+                            else '{:.4e}')
+        return magnitude_format.format(self.magnitude) + ' ' + ' * '.join(units)
